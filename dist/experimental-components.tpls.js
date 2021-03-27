341200860a311e376422af9fb4cc4e07815a7085
Darshan Patel <ddpatel97@gmail.com>
2021-03-27 20:17:01 +0530

    build prod 1.0.3

--- dist/experimental-components.tpls.js
+++ dist/experimental-components.tpls.js
@@ -5,7 +5,7 @@
         .factory('experiment',experiment);
     
         experiment.$inject = ['experimentConfig'];
-        function experiment() {
+        function experiment(experiment) {
 
             function printMsg(){
                 console.log("Sample Message from Package");
